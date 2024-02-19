7e626b2e4d6d2634b4f30f4260f7eb34fe405820
KaterinaB10 <148851754+KaterinaB10@users.noreply.github.com>
2024-02-19 12:25:03 +0100

    Update sounds.js

--- data/sounds.js
+++ data/sounds.js
@@ -2,51 +2,51 @@ export const sounds = [
   {
     file: "./sounds/exotic-disco.wav",
     key: "1",
-    background: "../public/images/disco.jpg",
+    background: "./public/images/disco.jpg",
   },
   {
     file: "./sounds/jazz.wav",
     key: "2",
-    background: "../public/images/quitar.jpg",
+    background: "./public/images/quitar.jpg",
   },
   {
     file: "./sounds/piano-classical.wav",
     key: "3",
-    background: "../public/images/piano.jpg",
+    background: "./public/images/piano.jpg",
   },
   {
     file: "./sounds/piano-melody-with-vocals.wav",
     key: "4",
-    background: "../public/images/lounge.jpg",
+    background: "./public/images/lounge.jpg",
   },
   {
     file: "./sounds/smooth-gospel.wav",
     key: "5",
-    background: "../public/images/smoke.jpg",
+    background: "./public/images/smoke.jpg",
   },
   {
     file: "./sounds/exotic-disco.wav",
     key: "6",
-    background: "../public/images/disco.jpg",
+    background: "./public/images/disco.jpg",
   },
   {
     file: "./sounds/jazz.wav",
     key: "7",
-    background: "../public/images/quitar.jpg",
+    background: "./public/images/quitar.jpg",
   },
   {
     file: "./sounds/piano-classical.wav",
     key: "8",
-    background: "../public/images/piano.jpg",
+    background: "./public/images/piano.jpg",
   },
   {
     file: "./sounds/piano-melody-with-vocals.wav",
     key: "9",
-    background: "../public/images/lounge.jpg",
+    background: "./public/images/lounge.jpg",
   },
   {
     file: "./sounds/smooth-gospel.wav",
     key: "0",
-    background: "../public/images/smoke.jpg",
+    background: "./public/images/smoke.jpg",
   },
 ];
