aa773702dfcec2f15ee66f72fc85cc93b6132684
KaterinaB10 <148851754+KaterinaB10@users.noreply.github.com>
2024-02-19 12:30:38 +0100

    Update soundsForth.js

--- data/soundsForth.js
+++ data/soundsForth.js
@@ -2,28 +2,28 @@ export const soundsForth = [
   {
     file: "./sounds/exotic-disco.wav",
     key: "z",
-    background: "../images/disco.jpg",
+    background: "./public/images/disco.jpg",
   },
-  { file: "./sounds/jazz.wav", key: "x", background: "../images/quitar.jpg" },
+  { file: "./sounds/jazz.wav", key: "x", background: "./public/images/quitar.jpg" },
   {
     file: "./sounds/piano-classical.wav",
     key: "c",
-    background: "../images/piano.jpg",
+    background: "./public/images/piano.jpg",
   },
   {
     file: "./sounds/piano-melody-with-vocals.wav",
     key: "v",
-    background: "../images/lounge.jpg",
+    background: "./public/images/lounge.jpg",
   },
   {
     file: "./sounds/smooth-gospel.wav",
     key: "b",
-    background: "../images/smoke.jpg",
+    background: "./public/images/smoke.jpg",
   },
   {
     file: "./sounds/exotic-disco.wav",
     key: "n",
-    background: "../images/disco.jpg",
+    background: "./public/images/disco.jpg",
   },
-  { file: "./sounds/jazz.wav", key: "m", background: "../images/quitar.jpg" },
+  { file: "./sounds/jazz.wav", key: "m", background: "./public/images/quitar.jpg" },
 ];
