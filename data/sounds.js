9b2bce2acae8016cd36191f2c13109b22dfc0b01
KaterinaB10 <katerina.b10.kodehode@gmail.com>
2024-02-19 12:19:42 +0100

    .

--- data/sounds.js
+++ data/sounds.js
@@ -2,43 +2,51 @@ export const sounds = [
   {
     file: "./sounds/exotic-disco.wav",
     key: "1",
-    background: "./public/images/disco.jpg",
+    background: "../public/images/disco.jpg",
   },
-  { file: "./sounds/jazz.wav", key: "2", background: "/images/quitar.jpg" },
+  {
+    file: "./sounds/jazz.wav",
+    key: "2",
+    background: "../public/images/quitar.jpg",
+  },
   {
     file: "./sounds/piano-classical.wav",
     key: "3",
-    background: "/images/piano.jpg",
+    background: "../public/images/piano.jpg",
   },
   {
     file: "./sounds/piano-melody-with-vocals.wav",
     key: "4",
-    background: "/images/lounge.jpg",
+    background: "../public/images/lounge.jpg",
   },
   {
     file: "./sounds/smooth-gospel.wav",
     key: "5",
-    background: "/images/smoke.jpg",
+    background: "../public/images/smoke.jpg",
   },
   {
     file: "./sounds/exotic-disco.wav",
     key: "6",
-    background: "/images/disco.jpg",
+    background: "../public/images/disco.jpg",
   },
-  { file: "./sounds/jazz.wav", key: "7", background: "/images/quitar.jpg" },
+  {
+    file: "./sounds/jazz.wav",
+    key: "7",
+    background: "../public/images/quitar.jpg",
+  },
   {
     file: "./sounds/piano-classical.wav",
     key: "8",
-    background: "/images/piano.jpg",
+    background: "../public/images/piano.jpg",
   },
   {
     file: "./sounds/piano-melody-with-vocals.wav",
     key: "9",
-    background: "/images/lounge.jpg",
+    background: "../public/images/lounge.jpg",
   },
   {
     file: "./sounds/smooth-gospel.wav",
     key: "0",
-    background: "/images/smoke.jpg",
+    background: "../public/images/smoke.jpg",
   },
 ];
